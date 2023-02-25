9f71311d9bf0594543db68ff1e8b19ae8ee49da9
StreamPay <contact@streamprotocol.org>
2023-02-25 12:30:26 +0200

    commit

--- b2b-storefront/next.config.js
+++ b2b-storefront/next.config.js
@@ -2,11 +2,11 @@ const { withStoreConfig } = require("./store-config")
 const store = require("./store.config.json")
 
 module.exports = withStoreConfig({
-  features: store.features,
-  reactStrictMode: true,
-  images: {
-    domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com", "localhost"],
-  },
+    features: store.features,
+    reactStrictMode: true,
+    images: {
+        domains: ["https://commerce.streampayment.app", "localhost"],
+    },
 })
 
 console.log("next.config.js", JSON.stringify(module.exports, null, 2))
